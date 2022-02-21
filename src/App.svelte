52f24e4c5f95ce90cee732064b1f8c81948292f4
Caleb Denio <cjdenio44@gmail.com>
2022-02-20 19:37:56 -0500

    sweet keyboard

--- src/App.svelte
+++ src/App.svelte
@@ -8,6 +8,7 @@
     Letter,
     mergeTemplateAndText,
   } from "./lib/util";
+  import Keyboard from "./lib/Keyboard.svelte";
 
   const start = new Date(2022, 1, 18, 0, 0, 0, 0);
   const channleIndex = Math.floor(
@@ -54,8 +55,20 @@
 
     if (!e.metaKey && !e.ctrlKey) {
       if (e.code === "Backspace") {
-        text = text.slice(0, -1);
-      } else if (e.code === "Enter" && isFull) {
+        handleKeyPress("backspace");
+      } else if (e.code === "Enter") {
+        handleKeyPress("enter");
+      } else if (e.code.startsWith("Key")) {
+        handleKeyPress(e.key);
+      }
+    }
+  }
+
+  async function handleKeyPress(key: "enter" | "backspace" | string) {
+    if (key === "backspace") {
+      text = text.slice(0, -1);
+    } else if (key === "enter") {
+      if (isFull) {
         const result = getResult(
           correct,
           rendered.map((i) => i.letter).join("")
@@ -69,9 +82,9 @@
         }
         await tick();
         scrollBox.scrollTop = scrollBox.scrollHeight;
-      } else if (e.code.startsWith("Key") && !isFull) {
-        text += e.key.toUpperCase();
       }
+    } else if (!isFull) {
+      text += key.toUpperCase();
     }
   }
 
@@ -87,37 +100,39 @@
 
 <svelte:window on:keydown={handleKeyDown} />
 
-<main class="pt-10">
-  <div class="mb-10">
-    <h1 class="text-5xl text-center font-bold">
-      {#if solved}
-        🎉 Channle 🎉
-      {:else}
-        Channle
-      {/if}
-    </h1>
-    <h3 class="text-sm text-center text-gray-700">It's not a typo</h3>
-  </div>
-
-  <div class="overflow-auto max-h-[600px]" bind:this={scrollBox}>
-    {#each historical as thing, index}
-      <Letters text={thing} />
-    {/each}
-
-    {#if !solved}
-      <Letters text={rendered} />
-    {/if}
-  </div>
-
-  {#if solved}
-    <div class="text-center mt-5">
-      <div>🎉 Congrats for solving today's Channle!</div>
-
-      <button
-        class="bg-green-700 font-bold px-3 py-2 mt-3 hover:bg-green-800 rounded-md"
-        on:click={() => shareResults()}
-        >{#if copied}✅ Copied to clipboard!{:else}Share results{/if}</button
-      >
+<main>
+  <div class="flex flex-col items-center h-screen gap-10">
+    <div class="mt-10">
+      <h1 class="text-5xl text-center font-bold">
+        {#if solved}
+          🎉 Channle 🎉
+        {:else}
+          Channle
+        {/if}
+      </h1>
+      <h3 class="text-sm text-center text-gray-700">It's not a typo</h3>
     </div>
-  {/if}
+
+    <div class="overflow-auto grow" bind:this={scrollBox}>
+      {#each historical as thing, index}
+        <Letters text={thing} />
+      {/each}
+
+      {#if !solved}
+        <Letters text={rendered} />
+      {/if}
+    </div>
+
+    {#if solved}
+      <div class="text-center">
+        <button
+          class="bg-green-700 font-bold px-3 py-2 hover:bg-green-800 rounded-md"
+          on:click={() => shareResults()}
+          >{#if copied}✅ Copied to clipboard!{:else}Share results{/if}</button
+        >
+      </div>
+    {/if}
+
+    <Keyboard on:keypress={(e) => handleKeyPress(e.detail.key)} />
+  </div>
 </main>
---
+++ src/lib/Keyboard.svelte
@@ -0,0 +1,199 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  function dispatchKeyPress(key: string) {
+    dispatch("keypress", {
+      key,
+    });
+  }
+</script>
+
+<div class="max-w-xl w-full flex gap-2 flex-col items-stretch mb-10">
+  <div class="flex gap-2">
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("q")}
+    >
+      Q
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("w")}
+    >
+      W
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("e")}
+    >
+      E
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("r")}
+    >
+      R
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("t")}
+    >
+      T
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("y")}
+    >
+      Y
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("u")}
+    >
+      U
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("i")}
+    >
+      I
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("o")}
+    >
+      O
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("p")}
+    >
+      P
+    </div>
+  </div>
+  <div class="flex gap-2">
+    <div class="basis-5 text-center rounded-md" />
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("a")}
+    >
+      A
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("s")}
+    >
+      S
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("d")}
+    >
+      D
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("f")}
+    >
+      F
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("g")}
+    >
+      G
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("h")}
+    >
+      H
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("j")}
+    >
+      J
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("k")}
+    >
+      K
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("l")}
+    >
+      L
+    </div>
+    <div class="basis-5 text-center rounded-md" />
+  </div>
+  <div class="flex gap-2">
+    <div
+      class="bg-gray-500 py-3 basis-0 grow-[2] text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("enter")}
+    >
+      Enter!
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("z")}
+    >
+      Z
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("x")}
+    >
+      X
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("c")}
+    >
+      C
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("v")}
+    >
+      V
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("b")}
+    >
+      B
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("n")}
+    >
+      N
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("m")}
+    >
+      M
+    </div>
+    <div
+      class="bg-gray-500 py-3 basis-0 grow-[2] text-center rounded-md cursor-pointer select-none"
+      on:click={() => dispatchKeyPress("backspace")}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24"
+        viewBox="0 0 24 24"
+        class="fill-gray-200 mx-auto"
+      >
+        <path
+          d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"
+        />
+      </svg>
+    </div>
+  </div>
+</div>
